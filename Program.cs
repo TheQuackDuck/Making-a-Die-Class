6334cedcbb500045938c1ebd8e91e845adfec677
363229626 <363229626@2260I-L29-022>
2024-04-12 12:21:58 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Making_a_Die_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Die die1 = new Die();
+            Die die2 = new Die();
+
+            Console.WriteLine(die1);
+            for(int i = 0; i < 10; i++)
+            {
+                die1.DrawRoll();
+                die1.RollDie();
+            }
+
+            Console.WriteLine(die1.Roll);
+            die1.DrawRoll();
+
+        }
+    }
+}
